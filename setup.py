a52676326c18ee8ba530b6738fec6e896d65fb87
Kawal Deol <35369346+KawalpreetDeol@users.noreply.github.com>
2024-01-15 08:51:04 -0500

    Readme setup.py fix

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+from pathlib import Path
+
 setup(name='stats_utils',
-      version='0.1',
-      description='Stats Utils library. Currently only Distributions',
+      version='0.1.1',
+      description='Probability and Statistics Utilities library. Currently only Distributions',
+      long_description=(Path(__file__).parent / "README.md").read_text(),
+      long_description_content_type="text/markdown",
       packages=['stats_utils'],
       author= 'Kawalpreet Deol',
       author_email='',
